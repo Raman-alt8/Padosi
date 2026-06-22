07123cd14423d0eabc933d3239b28aa4b0ec5f1f
raman <ramanchoudharymoney@gmail.com>
2026-06-22 19:57:38 +0530

    REVERSE

--- Java/scripts/scriptforPadosi.js
+++
@@ -1,9 +0,0 @@
-
-  const buttons = document.querySelectorAll('.schedule-toggle button');
-
-  buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-      buttons.forEach(b => b.classList.remove('active'));
-      btn.classList.add('active');
-    });
-  });
